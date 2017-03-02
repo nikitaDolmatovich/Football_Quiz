175e6f28d8c6024d09564a8d4fbdf7a424f1a8d9
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-03-02 22:41:30 +0300

    Create class QuizDialog with first configuration commit

---
+++ Football_Quiz/Bot.Backend/Logic/QuizDialog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+
+namespace Bot.Backend.Logic
+{
+    [Serializable]
+    public class QuizDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> arguments)
+        {
+            var message = await arguments;
+            await context.PostAsync("You said : " + message.Text);
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
