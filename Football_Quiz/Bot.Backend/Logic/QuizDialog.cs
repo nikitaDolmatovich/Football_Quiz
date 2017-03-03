c6ff7a75ffc2d22e42ad41b74b9c6990263defa1
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-03-03 12:18:26 +0300

    Add Welcome message commit

--- Football_Quiz/Bot.Backend/Logic/QuizDialog.cs
+++ Football_Quiz/Bot.Backend/Logic/QuizDialog.cs
@@ -5,6 +5,7 @@ using System.Web;
 using Microsoft.Bot.Builder.Dialogs;
 using System.Threading.Tasks;
 using Microsoft.Bot.Connector;
+using Bot.Backend.Resources;
 
 namespace Bot.Backend.Logic
 {
@@ -19,8 +20,29 @@ namespace Bot.Backend.Logic
         public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> arguments)
         {
             var message = await arguments;
-            await context.PostAsync("You said : " + message.Text);
-            context.Wait(MessageReceivedAsync);
+            var messageText = message.Text;
+            //await context.PostAsync("You said : " + message.Text);
+            //context.Wait(MessageReceivedAsync);
+
+            switch(messageText)
+            {
+                case "/start":
+                    var startMenu = GetStartMenu();
+                    await context.PostAsync(startMenu); 
+                    context.Wait(MessageReceivedAsync);
+                    break;
+            }
+        }
+
+        private string GetStartMenu()
+        {
+            var helloString = "Добро пожаловать в футбольную викторину!\n" +
+                        $"\n/start - {ResourceBot.MainMenu}\n" +
+                        $"\n/play - {ResourceBot.StartGame}\n" +
+                        $"\n/thematic - {ResourceBot.ThematicGame}\n" +
+                        $"\n/stat - {ResourceBot.Statistics}";
+
+            return helloString;
         }
 
     }
