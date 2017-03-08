ad3da9a296877783d9e1f5e8dfa562cf109b1897
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-03-08 21:49:40 +0300

    Update model and create interface Irepository commit

---
+++ Football_Quiz/Bot.Backend/Models/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Backend.Models
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(string name);
+    }
+}
