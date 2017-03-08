fd5c00e86ff33143cca97af801153b984605fb50
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-03-08 22:57:53 +0300

    Add class Condition commit

---
+++ Football_Quiz/Bot.Backend/Logic/Condition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot.Backend.Logic
+{
+    [Serializable]
+    public class Condition
+    {
+        public string CurrentChampionat { get; set; }
+        public string CurrentQuestion { get; set; }
+    }
+}
