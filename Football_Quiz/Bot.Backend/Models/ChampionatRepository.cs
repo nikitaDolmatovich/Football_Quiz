83852d157e82accc60970f2886d7da480f99f558
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-03-08 21:56:05 +0300

    Create ChampionatRespoitory commit

---
+++ Football_Quiz/Bot.Backend/Models/ChampionatRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot.Backend.Models
+{
+    public class ChampionatRepository : IRepository<Championat>
+    {
+        private BotContext context;
+
+        public ChampionatRepository(BotContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Championat> GetAll()
+        {
+            return context.Championats;
+        }
+
+        public Championat Get(string name)
+        {
+            var dbEntry = context.Championats.FirstOrDefault(x => x.ChampionatName == name);
+
+            if(dbEntry != null)
+            {
+                return dbEntry;
+            }
+            else
+            {
+                NullReferenceException ex = new NullReferenceException();
+                throw ex;
+            }
+        }
+    }
+}
