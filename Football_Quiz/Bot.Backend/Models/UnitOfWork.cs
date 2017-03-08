d06c7542633c13d1855901d1d41a084138932192
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-03-08 22:19:35 +0300

    Add class Unit Of Work commit

---
+++ Football_Quiz/Bot.Backend/Models/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot.Backend.Models
+{
+    public class UnitOfWork : IDisposable
+    {
+        private BotContext context = new BotContext();
+        private ChampionatRepository championatRepo;
+        private bool disposed = false;
+
+        public ChampionatRepository Championat
+        {
+            get
+            {
+                if (championatRepo == null)
+                {
+                    championatRepo = new ChampionatRepository(context);
+                }
+                return championatRepo;
+            }
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if(!this.disposed)
+            {
+                if(disposing)
+                {
+                    context.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
